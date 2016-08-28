7c9e416d3a46ccc2f18029e5bcfe0ca636a9e017
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-08-28 13:55:48 -0400

    code organization

--- api/api/user/index.js
+++ api/api/user/index.js
@@ -1,1 +1,15 @@
-module.exports = require('./controller');
+'use strict';
+
+const express = require('express');
+
+const auth = require('../../auth/auth.service');
+
+const controller = require('./controller');
+
+var router = new express.Router();
+
+router.get('/', auth.isAuthenticated(), controller.showCurrentUser);
+router.get('/token', auth.initialAuth(), controller.createToken);
+router.post('/', controller.createNewUser);
+
+module.exports = router;
--- api/app.js
+++ api/app.js
@@ -2,13 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const UserController = require('./api/user');
-const TokenController = require('./api/token');
 const passport = require('passport');
-const auth = require('./auth');
-const BasicStrategy = require('passport-http').BasicStrategy;
-const JWTStrategy = require('passport-jwt').Strategy;
-const ExtractJWT = require('passport-jwt').ExtractJwt;
 const logger = require('./logger');
 const config = require('./config');
 
@@ -16,30 +10,13 @@ const app = express();
 
 const jsonParser = bodyParser.json();
 
-const JWTOptions = {
-    jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('Bearer'),
-    secretOrKey: config.jwt.secret
-};
-
-passport.use(new BasicStrategy(auth.basicStrategy));
-passport.use(new JWTStrategy(JWTOptions, auth.jwtStrategy));
-
 /* Middleware */
 if (process.env.NODE_ENV === 'development') app.use(logger);
 app.use(jsonParser);
 app.use(passport.initialize());
 app.use(passport.session());
 
-//
-// User Routes
-//
-app.post('/api/v1.0/user', jsonParser, UserController.createNewUser);
-app.get('/api/v1.0/user/token', passport.authenticate('basic', {
-    session: false
-}), TokenController.create);
-app.get('/api/v1.0/user', passport.authenticate('jwt', {
-    session: false
-}), UserController.showCurrentUser);
-
+// Routes
+require('./routes')(app);
 
 module.exports = app;
