7f8f1f263a4be3ba5090831ab683aaf46e13f920
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-08-28 20:06:09 -0400

    move password hashing model instance level, asynchronize

--- api/api/user/model.js
+++ api/api/user/model.js
@@ -9,12 +9,50 @@ module.exports = function (sequelize, DataTypes) {
         admin: DataTypes.BOOLEAN
     }, {
         timestamps: false,
-        classMethods: {
-            hashPassword: function(password) {
-                return bcrypt.hashSync(password, 10);
+        hooks: {
+            beforeBulkCreate: function (users, fields, fn) {
+                var totalUpdated = 0;
+                users.forEach(function (user) {
+                    user.updatePassword(function (err) {
+                        if (err) {
+                            return fn(err);
+                        }
+                        totalUpdated += 1;
+                        if (totalUpdated === users.length) {
+                            return fn();
+                        }
+                    });
+                });
             },
+            beforeCreate: function (user, fields, fn) {
+                user.updatePassword(fn);
+            },
+            beforeUpdate: function (user, fields, fn) {
+                if (user.changed('password')) {
+                    return user.updatePassword(fn);
+                }
+                fn();
+            }
+        },
+        classMethods: {
             comparePassword: function(password, hash) {
                 return bcrypt.compareSync(password, hash);
+            }
+        },
+        instanceMethods: {
+            updatePassword: function (fn) {
+                // Handle new/update passwords
+                var value = this.password;
+                if (! value) {
+                    fn(new Error('Invalid password'));
+                }
+                bcrypt.hash(value, 10, (err, hash) => {
+                    if (err) {
+                        return fn(err);
+                    }
+                    this.password = hash;
+                    fn(null);
+                })
             }
         }
     });
