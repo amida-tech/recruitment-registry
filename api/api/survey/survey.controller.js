5cda625a814dfc9943648814e2945fd2ddd884af
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-09-01 12:57:22 -0400

    empty survey load api + integration test

--- api/api/survey/survey.controller.js
+++ api/api/survey/survey.controller.js
@@ -6,7 +6,8 @@ const Survey = db.Survey;
 
 exports.getEmptySurvey = function (req, res) {
     var name = req.params.name;
-    Survey.getEmptySurvey(name).then(function (survey) {
+    Survey.getSurveyByName(name).then(function (survey) {
+        survey.name = survey.name;
         res.status(200).json(survey);
     }).catch(function (err) {
         res.status(401).send(err);
