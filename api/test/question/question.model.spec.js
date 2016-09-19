b7db649c12ff7753fc5d20a2f136c556fc20100d
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-09-19 18:07:32 -0400

    get all questions, delete question are implemented on the model level

--- api/test/question/question.model.spec.js
+++ api/test/question/question.model.spec.js
@@ -37,6 +37,9 @@ describe('question unit', function () {
     }, {
         text: 'Where were you born?',
         type: 'text'
+    }, {
+        text: 'Do you have pets?',
+        type: 'bool'
     }];
 
     const cleanServerQuestion = function (question) {
@@ -49,49 +52,65 @@ describe('question unit', function () {
 
     const ids = [];
 
-    it('create/get multiple choice question with multiple answers (checkboxes)', function () {
-        return Question.createQuestion(examples[0])
-            .then(id => {
-                ids.push(id);
-                return Question.getQuestion(id);
-            })
-            .then(actual => {
-                cleanServerQuestion(actual);
-                expect(actual).to.deep.equal(examples[0]);
-            });
-    });
+    const qxBasicFn = function (index) {
+        return function () {
+            return Question.createQuestion(examples[index])
+                .then(id => {
+                    ids.push(id);
+                    return Question.getQuestion(id);
+                })
+                .then(actual => {
+                    cleanServerQuestion(actual);
+                    expect(actual).to.deep.equal(examples[index]);
+                })
+                .then(() => {
+                    const text = `Updated ${examples[index]}`;
+                    return Question.updateQuestion(ids[index], { text });
+                })
+                .then(() => {
+                    const id = ids[index];
+                    return Question.getQuestion(id);
+                })
+                .then(actual => {
+                    cleanServerQuestion(actual);
+                    const expected = _.cloneDeep(examples[index]);
+                    expected.text = `Updated ${examples[index]}`;
+                    expect(actual).to.deep.equal(expected);
+                })
+                .then(() => {
+                    const text = examples[index].text;
+                    return Question.updateQuestion(ids[index], { text });
+                });
+        };
+    };
 
-    it('create/get multiple choice question with single answer (drop down)', function () {
-        return Question.createQuestion(examples[1])
-            .then(id => {
-                ids.push(id);
-                return Question.getQuestion(id);
-            })
-            .then(actual => {
-                cleanServerQuestion(actual);
-                expect(actual).to.deep.equal(examples[1]);
-            });
-    });
+    for (let i = 0; i < examples.length; ++i) {
+        it(`create/get/update question ${i} type ${examples[i].type}`, qxBasicFn(i));
+    }
 
-    it('create/get text question', function () {
-        return Question.createQuestion(examples[2])
-            .then(id => {
-                ids.push(id);
-                return Question.getQuestion(id);
-            })
-            .then(actual => {
-                cleanServerQuestion(actual);
-                expect(actual).to.deep.equal(examples[2]);
-            });
-    });
+    const multipleQuestionVerify = function (indices = _.range(examples.length)) {
+        return function (questions) {
+            const testIds = _.pullAt(ids.slice(), indices);
+            const samples = _.pullAt(examples.slice(), indices);
+            return helper.buildServerQuestions(samples, testIds)
+                .then(expected => {
+                    expect(questions).to.deep.equal(expected);
+                });
+        };
+    };
 
     it('get multiple questions', function () {
-        return Question.getQuestions(ids)
-            .then(questions => {
-                return helper.buildServerQuestions(examples, ids)
-                    .then(expected => {
-                        expect(questions).to.deep.equal(expected);
-                    });
-            });
+        return Question.getQuestions(ids).then(multipleQuestionVerify());
+    });
+
+    it('get all questions', function () {
+        return Question.getAllQuestions().then(multipleQuestionVerify());
+    });
+
+    it('remove some question and verify', function () {
+        return Question.deleteQuestion(ids[1])
+            .then(() => Question.deleteQuestion(ids[3]))
+            .then(() => Question.getAllQuestions())
+            .then(multipleQuestionVerify([0, 2]));
     });
 });
