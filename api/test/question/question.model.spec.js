7e34faed1e29fc8488f02a38940b1cbf3c3fa63e
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-09-19 14:02:45 -0400

    code cleaning

--- api/test/question/question.model.spec.js
+++ api/test/question/question.model.spec.js
@@ -7,27 +7,14 @@ const _ = require('lodash');
 
 const helper = require('../helpers');
 const models = require('../../models');
+const shared = require('../shared.spec.js');
 
 const expect = chai.expect;
 
-const QuestionType = models.QuestionType;
-const QuestionChoices = models.QuestionChoices;
 const Question = models.Question;
 
 describe('question unit', function () {
-    before(function () {
-        return QuestionType.sync({
-            force: true
-        }).then(function () {
-            return QuestionChoices.sync({
-                force: true
-            });
-        }).then(function () {
-            return Question.sync({
-                force: true
-            });
-        });
-    });
+    before(shared.setUpFn());
 
     const examples = [{
         text: 'Which sports do you like?',
@@ -62,41 +49,49 @@ describe('question unit', function () {
 
     const ids = [];
 
-    it('post/get multiple choice question with multiple answers (checkboxes)', function () {
-        return Question.createQuestion(examples[0]).then(function (id) {
-            ids.push(id);
-            return Question.getQuestion(id);
-        }).then(function (actual) {
-            cleanServerQuestion(actual);
-            expect(actual).to.deep.equal(examples[0]);
-        });
+    it('create/get multiple choice question with multiple answers (checkboxes)', function () {
+        return Question.createQuestion(examples[0])
+            .then(id => {
+                ids.push(id);
+                return Question.getQuestion(id);
+            })
+            .then(actual => {
+                cleanServerQuestion(actual);
+                expect(actual).to.deep.equal(examples[0]);
+            });
     });
 
-    it('post/get multiple choice question with single answer (drop down)', function () {
-        return Question.createQuestion(examples[1]).then(function (id) {
-            ids.push(id);
-            return Question.getQuestion(id);
-        }).then(function (actual) {
-            cleanServerQuestion(actual);
-            expect(actual).to.deep.equal(examples[1]);
-        });
+    it('create/get multiple choice question with single answer (drop down)', function () {
+        return Question.createQuestion(examples[1])
+            .then(id => {
+                ids.push(id);
+                return Question.getQuestion(id);
+            })
+            .then(actual => {
+                cleanServerQuestion(actual);
+                expect(actual).to.deep.equal(examples[1]);
+            });
     });
 
-    it('post/get text question', function () {
-        return Question.createQuestion(examples[2]).then(function (id) {
-            ids.push(id);
-            return Question.getQuestion(id);
-        }).then(function (actual) {
-            cleanServerQuestion(actual);
-            expect(actual).to.deep.equal(examples[2]);
-        });
+    it('create/get text question', function () {
+        return Question.createQuestion(examples[2])
+            .then(id => {
+                ids.push(id);
+                return Question.getQuestion(id);
+            })
+            .then(actual => {
+                cleanServerQuestion(actual);
+                expect(actual).to.deep.equal(examples[2]);
+            });
     });
 
     it('get multiple questions', function () {
-        return Question.getQuestions(ids).then(function (questions) {
-            return helper.buildServerQuestions(examples, ids).then(function (expected) {
-                expect(questions).to.deep.equal(expected);
+        return Question.getQuestions(ids)
+            .then(questions => {
+                return helper.buildServerQuestions(examples, ids)
+                    .then(expected => {
+                        expect(questions).to.deep.equal(expected);
+                    });
             });
-        });
     });
 });
