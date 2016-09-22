6e65d0067f1e8f035794c0132a1f2eb393a7d68f
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-09-21 22:34:31 -0400

    create survey, list survey, show survey use case

--- api/test/use-cases/create-show-survey.integration.js
+++ api/test/use-cases/create-show-survey.integration.js
@@ -2,12 +2,19 @@
 'use strict';
 process.env.NODE_ENV = 'test';
 
+const chai = require('chai');
+
+const helper = require('../helper/survey-helper');
+
 const shared = require('../shared-integration');
 const registryExamples = require('../fixtures/registry-examples');
+const surveyExamples = require('../fixtures/survey-examples');
 
 const config = require('../../config');
 
-describe('create-show-survey', function () {
+const expect = chai.expect;
+
+describe('create-show-survey use case', function () {
     const registryExample = registryExamples[0];
 
     // -------- set up system (syncAndLoadAlzheimer)
@@ -25,8 +32,73 @@ describe('create-show-survey', function () {
 
     it('logout as super user', shared.logoutFn(store));
 
-    // --------
+    // -------- only admin's create surveys
 
-    // -------- see only Alzhimers survey list
+    it('login as super user', shared.loginFn(store, config.superUser));
 
+    // -------- see only Alzheimers in the survey list
+
+    it('list surveys to see Alzheimers for users profile', function (done) {
+        store.server
+            .get('/api/v1.0/surveys')
+            .set('Authorization', store.auth)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                const surveys = res.body;
+                expect(surveys).to.have.length(1);
+                expect(surveys[0].name).to.equal(registryExample.survey.name);
+                done();
+            });
+    });
+
+    //-------- create another survey
+
+    it('create a new survey', function (done) {
+        store.server
+            .post('/api/v1.0/surveys')
+            .set('Authorization', store.auth)
+            .send(surveyExamples.Example.survey)
+            .expect(201, done);
+    });
+
+    //------- list surveys and select one to shoe
+
+    it('list surveys to see the new survey', function (done) {
+        store.server
+            .get('/api/v1.0/surveys')
+            .set('Authorization', store.auth)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                const surveys = res.body;
+                expect(surveys).to.have.length(2);
+                expect(surveys[0].name).to.equal(registryExample.survey.name);
+                expect(surveys[1].name).to.equal(surveyExamples.Example.survey.name);
+                store.lastId = surveys[1].id;
+                done();
+            });
+    });
+
+    it('show the new survey', function (done) {
+        store.server
+            .get(`/api/v1.0/surveys/${store.lastId}`)
+            .set('Authorization', store.auth)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                helper.buildServerSurveyFromClientSurvey(surveyExamples.Example.survey, res.body)
+                    .then(function (expected) {
+                        expect(res.body).to.deep.equal(expected);
+                    })
+                    .then(done)
+                    .catch(done);
+            });
+    });
 });
