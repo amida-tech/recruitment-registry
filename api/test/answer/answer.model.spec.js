aa29872ae81fc38f0e20810aa3b0b7d3b8c274d4
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-09-23 12:11:56 -0400

    unit tests for updating answers

--- api/test/answer/answer.model.spec.js
+++ api/test/answer/answer.model.spec.js
@@ -6,6 +6,7 @@ const _ = require('lodash');
 
 const models = require('../../models');
 const shared = require('../shared-spec');
+const jsutil = require('../../lib/jsutil');
 
 const expect = chai.expect;
 
@@ -36,51 +37,51 @@ describe('answer unit', function () {
         [14, 15, 16, 17, 18, 19]
     ];
 
-    //const testQuestions = [{
-    //    survey: [0, 1, 2, 3, 4],
-    //    answerSequences: [
-    //        [
-    //            [0, 1, 2, 3, 4],
-    //            [0, 1, 2, 3, 4]
-    //        ]
-    //    ]
-    //}, {
-    //    survey: [4, 5, 6, 0],
-    //    answerSequences: [
-    //        [
-    //            [4, 5, 6, 0],
-    //            [4, -6]
-    //        ]
-    //    ]
-    //}, {
-    //    survey: [7, 8, 9, 10, 11, 12],
-    //    answerSequences: [
-    //        [
-    //            [8, 10, 11, 12],
-    //            [7, 10, -12]
-    //        ]
-    //    ]
-    //}, {
-    //    survey: [9, 11, 13, 6],
-    //    answerSequences: [
-    //        [
-    //            [9, 13],
-    //            [6, 11]
-    //        ],
-    //        [
-    //            [9, 11, 13, 6],
-    //            [9, 11, -6]
-    //        ]
-    //    ]
-    //}, {
-    //    survey: [14, 15, 16, 17, 18, 19],
-    //    answerSequences: [
-    //        [
-    //            [14, 15, 16, 17, 18, 19],
-    //            [-15, 16, -17, -19]
-    //        ]
-    //    ]
-    //}];
+    const testQuestions = [{
+        survey: [0, 1, 2, 3, 4],
+        answerSequences: [
+            [
+                [0, 1, 2, 3, 4],
+                [0, 1, 2, 3, 4]
+            ]
+        ]
+    }, {
+        survey: [4, 5, 6, 0],
+        answerSequences: [
+            [
+                [4, 5, 6, 0],
+                [4, -6]
+            ]
+        ]
+    }, {
+        survey: [7, 8, 9, 10, 11, 12],
+        answerSequences: [
+            [
+                [8, 10, 11, 12],
+                [7, 10, -12]
+            ]
+        ]
+    }, {
+        survey: [9, 11, 13, 6],
+        answerSequences: [
+            [
+                [9, 13],
+                [6, 11]
+            ],
+            [
+                [9, 11, 13, 6],
+                [9, 11, -6]
+            ]
+        ]
+    }, {
+        survey: [14, 15, 16, 17, 18, 19],
+        answerSequences: [
+            [
+                [14, 15, 16, 17, 18, 19],
+                [-15, 16, -17, -19]
+            ]
+        ]
+    }];
 
     surveyQuestions.forEach((surveyQuestion, index) => {
         return it(`create survey ${index}`, shared.createSurvey(store, surveyQuestion));
@@ -153,10 +154,22 @@ describe('answer unit', function () {
         };
     })();
 
+    const updateHxAnswers = function (key, qxIndices, answers) {
+        const hx = store.hxAnswers[key] || (store.hxAnswers[key] = []);
+        const qxAnswers = answers.reduce((r, answer, index) => {
+            const qxIndex = qxIndices[index];
+            if (qxIndex >= 0) {
+                r[qxIndex] = answer;
+            }
+            return r;
+        }, {});
+        hx.push({ qxIndices, qxAnswers });
+    };
+
     const createTestFn = function (userIndex, surveyIndex, qxIndices, key) {
         return function () {
             const answers = qxIndices.map(genQuestionAnswer);
-            store.hxAnswers[key] = [{ answers, qxIndices }];
+            updateHxAnswers(key, qxIndices, answers);
             const input = {
                 userId: store.users[userIndex],
                 surveyId: store.surveys[surveyIndex],
@@ -184,34 +197,47 @@ describe('answer unit', function () {
     it('user 2 answers survey 4 create full', createTestFn(2, 4, [14, 15, 16, 17, 18, 19], 'e'));
     it('user 0 answers survey 3 create full', createTestFn(0, 3, [9, 11, 13, 6], 'f'));
 
-    //const updateTestFn = function (userIndex, surveyIndex, qxIndices, key) {
-    //return function () {
-    //    const answers = qxIndices.map(genQuestionAnswer);
-    //    store.updateAnswers[key].push({ answers, qxIndices });
-    //    const input = {
-    //        userId: store.users[userIndex],
-    //        surveyId: store.surveys[surveyIndex],
-    //        answers
-    //    };
-    //    return models.Answer.updateAnswers(input)
-    //        .then(function () {
-    //            return models.Answer.getSurveyAnswers({
-    //                    userId: store.users[userIndex],
-    //                    surveyId: store.surveys[surveyIndex]
-    //                })
-    //                .then(function (result) {
-    //                    const expected = _.sortBy(answers, 'questionId');
-    //                    const actual = _.sortBy(result, 'questionId');
-    //                    expect(actual).to.deep.equal(expected);
-    //                });
-    //        });
-    //};
-    //};
+    const pullExpectedAnswers = function (key) {
+        const answersSpec = store.hxAnswers[key];
+        const standing = jsutil.findStanding(_.map(answersSpec, 'qxIndices'));
+        return standing.reduce((r, answerIndices, index) => {
+            answerIndices.forEach((answerIndex) => {
+                const answer = answersSpec[index].qxAnswers[answerIndex];
+                r.push(answer);
+            });
+            return r;
+        }, []);
+    };
 
-    //xit('user 0 answers survey 0 answer 1', updateTestFn(0, 0, testQuestions.answerSequences[0][1], 'a'));
-    //xit('user 1 answers survey 1 answer 1', updateTestFn(1, 1, testQuestions.answerSequences[1][1], 'b'));
-    //xit('user 2 answers survey 2 answer 1', updateTestFn(2, 2, testQuestions.answerSequences[2][1], 'c'));
-    //xit('user 3 answers survey 3 answer 1', updateTestFn(3, 3, testQuestions.answerSequences[3][1], 'd'));
-    //xit('user 2 answers survey 4 answer 1', updateTestFn(2, 4, testQuestions.answerSequences[4][1], 'e'));
-    //xit('user 0 answers survey 3 answer 1', updateTestFn(0, 3, testQuestions.answerSequences[3][1], 'f'));
+    const updateTestFn = function (userIndex, surveyIndex, qxIndices, key) {
+        return function () {
+            const answers = qxIndices.map(genQuestionAnswer);
+            updateHxAnswers(key, qxIndices, answers);
+            const input = {
+                userId: store.users[userIndex],
+                surveyId: store.surveys[surveyIndex],
+                answers
+            };
+            return models.Answer.updateAnswers(input)
+                .then(function () {
+                    return models.Answer.getSurveyAnswers({
+                            userId: store.users[userIndex],
+                            surveyId: store.surveys[surveyIndex]
+                        })
+                        .then(function (result) {
+                            const expectedAnswers = pullExpectedAnswers(key);
+                            const expected = _.sortBy(expectedAnswers, 'questionId');
+                            const actual = _.sortBy(result, 'questionId');
+                            expect(actual).to.deep.equal(expected);
+                        });
+                });
+        };
+    };
+
+    it('user 0 answers survey 0 answer 1', updateTestFn(0, 0, testQuestions[0].answerSequences[0][1], 'a'));
+    it('user 1 answers survey 1 answer 1', updateTestFn(1, 1, testQuestions[1].answerSequences[0][1], 'b'));
+    it('user 2 answers survey 2 answer 1', updateTestFn(2, 2, testQuestions[2].answerSequences[0][1], 'c'));
+    it('user 3 answers survey 3 answer 1', updateTestFn(3, 3, testQuestions[3].answerSequences[0][1], 'd'));
+    it('user 2 answers survey 4 answer 1', updateTestFn(2, 4, testQuestions[4].answerSequences[0][1], 'e'));
+    it('user 0 answers survey 3 answer 1', updateTestFn(0, 3, testQuestions[3].answerSequences[1][1], 'f'));
 });
