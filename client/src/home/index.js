90c6e2cc3db895076f271d1459ab32f6f49df2a2
Jon Berry <jon@jonberry.co>
2016-10-19 15:53:11 -0400

    updated interface elements, including selects and text inputs

--- client/src/home/index.js
+++ client/src/home/index.js
@@ -8,6 +8,7 @@ class Home extends Component {
     const loggedIn = this.props.data.get('loggedIn');
     const role = this.props.user.get('role');
     const username = this.props.user.get('username');
+    document.getElementsByTagName('body')[0].className+=' ';
     return (
       <div className="home">
         <h1>{this.props.vocab.get('HOME')}</h1>
@@ -16,7 +17,17 @@ class Home extends Component {
             <h6> <span style={{color: "#2196F3"}}>{ username }</span>, {this.props.vocab.get('WELCOME')} { title }, {this.props.vocab.get('LOGGED_IN_AS')} <span style={{color: "#2196F3"}}>{ role }</span>!</h6>
           ) : (<div></div>)
           }
+
         </div>
+        <form className="form" onSubmit={this.props.onSubmit}>
+            <input className="form__field-input" id="password" name="fuck" type="radio" onChange={ this.props.changeForm } placeholder={this.props.vocab.get('PASSWORD')} />
+            <input className="form__field-input" id="password" name="fuck" type="radio" onChange={ this.props.changeForm } placeholder={this.props.vocab.get('PASSWORD')} />
+        </form>
+        <select className="form-select">
+          <option value="A">I'm not sure</option>
+          <option value="B">Banana</option>
+          <option value="C">Cranberry</option>
+        </select>
       </div>
     );
   }
