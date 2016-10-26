a510bf4b2e48ba2a9ba4040a5c81fe6248741812
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-10-25 22:03:58 -0400

    merge reset-token tests

--- api/test/use-cases/reset-password.integration.js
+++ api/test/use-cases/reset-password.integration.js
@@ -1,9 +1,12 @@
-/* global describe,before,it*/
+/* global describe,before,after,it*/
 'use strict';
 process.env.NODE_ENV = 'test';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const stream = require('stream');
 
 const chai = require('chai');
-const sinon = require('sinon');
+const smtpServer = require('smtp-server');
 
 const helper = require('../util/survey-common');
 
@@ -12,7 +15,6 @@ const userExamples = require('../fixtures/example/user');
 const surveyExamples = require('../fixtures/example/survey');
 
 const config = require('../../config');
-const mailer = require('../../lib/mailer');
 const RRError = require('../../lib/rr-error');
 
 const expect = chai.expect;
@@ -29,7 +31,54 @@ describe('reset password use-case', function () {
         auth: null
     };
 
+    const receivedEmail = {
+        auth: null,
+        from: null,
+        to: null,
+        content: ''
+    };
+
+    class SMTPStream extends stream.Writable {
+        _write(chunk, enc, next) {
+            receivedEmail.content += chunk.toString();
+            next();
+        }
+    }
+
+    const smtpStream = new SMTPStream();
+
+    const server = new smtpServer.SMTPServer({
+        name: 'localhost',
+        authOptional: true,
+        onAuth: function (auth, session, callback) {
+            receivedEmail.auth = auth;
+            callback(null, {
+                user: 1
+            });
+        },
+        onMailFrom: function (address, session, callback) {
+            receivedEmail.from = address.address;
+            if (address.address.indexOf('smtp') >= 0) {
+                return callback(null);
+            } else {
+                return callback(new Error('invalid'));
+            }
+        },
+        onRcptTo: function (address, session, callback) {
+            receivedEmail.to = address.address;
+            callback();
+        },
+        onData: function (stream, session, callback) {
+            stream.pipe(smtpStream);
+            stream.on('end', callback);
+        }
+    });
+
     before(shared.setUpFn(store));
+
+    it('start smtp server', function () {
+        server.listen(9001);
+    });
 
     it('login as super user', shared.loginFn(store, config.superUser));
 
@@ -76,7 +125,7 @@ describe('reset password use-case', function () {
 
     it('verify user can login', shared.loginFn(store, userExample));
 
-    let token;
+    let token = null;
 
     it('error: no smtp settings is specified', function (done) {
         store.server
@@ -101,7 +150,7 @@ describe('reset password use-case', function () {
         username: 'smtp@example.com',
         password: 'pw',
         host: 'localhost',
-        from: userExample.email,
+        from: 'admin@rr.com',
         otherOptions: {}
     };
 
@@ -150,32 +199,34 @@ describe('reset password use-case', function () {
     it('logout as super', shared.logoutFn(store));
 
     it('error: generate reset tokens', function (done) {
-        const stub = sinon.stub(mailer, 'sendEmail', function (uri, options, callback) {
-            callback(new Error(options.subject));
-        });
         store.server
             .post('/api/v1.0/reset-tokens')
             .send({
                 email: userExample.email
             })
             .expect(500)
-            .end(function (err, res) {
+            .end(function (err) {
                 if (err) {
                     return done(err);
                 }
-                expect(stub.called).to.equal(true);
-                expect(!!res.body.message).to.equal(true);
-                mailer.sendEmail.restore();
                 done();
             });
     });
 
+    it('login as super', shared.loginFn(store, config.superUser));
+
+    it('setup server specifications', function (done) {
+        smtpSpec.from = 'smtp@rr.com';
+        store.server
+            .post('/api/v1.0/smtp')
+            .set('Authorization', store.auth)
+            .send(smtpSpec)
+            .expect(204, done);
+    });
+
+    it('logout as super', shared.logoutFn(store));
+
     it('generate reset tokens', function (done) {
-        const stub = sinon.stub(mailer, 'sendEmail', function (uri, options, callback) {
-            const linkPieces = options.text.split('/');
-            token = linkPieces[linkPieces.length - 1];
-            callback(null);
-        });
         store.server
             .post('/api/v1.0/reset-tokens')
             .send({
@@ -186,14 +237,36 @@ describe('reset password use-case', function () {
                 if (err) {
                     return done(err);
                 }
-                expect(stub.called).to.equal(true);
-                mailer.sendEmail.restore();
-                expect(!!token).to.equal(true);
                 done();
             });
     });
 
     it('verify user can not login', shared.badLoginFn(store, userExample));
+
+    it('checked received email and recover token', function () {
+        expect(receivedEmail.auth.username).to.equal(smtpSpec.username);
+        expect(receivedEmail.auth.password).to.equal(smtpSpec.password);
+        expect(receivedEmail.from).to.equal(smtpSpec.from);
+        expect(receivedEmail.to).to.equal(userExample.email);
+        const lines = receivedEmail.content.split('\r\n');
+        let subjectFound = false;
+        lines.forEach((line, index) => {
+            if (line.startsWith('Subject: ')) {
+                const subject = line.split('Subject: ')[1];
+                expect(subject).to.equal(smtpText.subject);
+                subjectFound = true;
+            }
+            if (line.startsWith('Click on this:')) {
+                const linkPieces = line.split('/');
+                token = linkPieces[linkPieces.length - 1];
+                if (token.charAt(token.length - 1) === '=') {
+                    token = token.slice(0, token.length - 1) + lines[index + 1];
+                }
+            }
+        });
+        expect(subjectFound).to.equal(true);
+        expect(token).to.not.equal(null);
+    });
 
     it('reset password', function (done) {
         store.server
@@ -211,4 +284,8 @@ describe('reset password use-case', function () {
         username: userExample.username,
         password: 'newPassword'
     }));
+
+    after(function (done) {
+        server.close(done);
+    });
 });
