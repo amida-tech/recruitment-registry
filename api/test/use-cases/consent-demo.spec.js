fdbe57985ec257fba3ce4051d5d92ef62eb3ab72
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-10-27 18:14:33 -0400

    document consent usage

--- api/test/use-cases/consent-demo.spec.js
+++ api/test/use-cases/consent-demo.spec.js
@@ -23,6 +23,11 @@ describe('consent unit', function () {
     };
     const hxUser = new History();
 
+    //*******
+    // Sync and seed the database.  This is part of syncAndLoadAlzheimer.js script.  It creates the consent documents.
+    // The document contents themselves are in test/fixtures/example/consent-demo.  Change the content however you wish.
+    //******* START 1
+
     before(shared.setUpFn(store));
 
     it('create Terms of Use and Consent Form records', function () {
@@ -30,6 +35,12 @@ describe('consent unit', function () {
     });
 
     let termsOfUse;
+
+    //****** END 1
+
+    //******
+    // Get Terms of Use before regustration.  The content will be in res.body.sections[0].content.  Show it to user
+    //****** START 2
 
     it('get Terms of Use before registration', function (done) {
         store.server
@@ -48,6 +59,15 @@ describe('consent unit', function () {
 
     });
 
+    //****** END 2
+
+    //******
+    // At this point user is not created.  It will be created when registration is sent dowm.  Right now
+    // marking the terms of use as signed only possible after user is created so on the client you will 
+    // have to first get the results of registration (which will get you the authentication token) and send 
+    // down the signatures.  User creation below simulates the registration.
+    //****** START 3
+
     it('login as super', shared.loginFn(store, config.superUser));
 
     const user = generator.newUser();
@@ -56,6 +76,8 @@ describe('consent unit', function () {
     it('logout as super', shared.logoutFn(store));
 
     it('login as user', shared.loginFn(store, user));
+
+    // This us the actual signing of the terms of use document
 
     it('sign the Terms of Use document', function (done) {
         store.server
@@ -70,6 +92,13 @@ describe('consent unit', function () {
                 done();
             });
     });
+
+    //****** END 3
+
+    //******
+    // Later you can always get the Terms of use and check if a new signature is needed.
+    // New signature will be needed if a new Terms of Use document is posted (admin functionality, we can simulate from a script if needed).
+    //****** START 4
 
     it('get the Terms of Use document with signature', function (done) {
         store.server
@@ -87,7 +116,14 @@ describe('consent unit', function () {
             });
     });
 
+    //****** END 4
+
     let consents;
+
+    //******
+    // Get the Consent Form and check if it is signed.  Again the content is in res.body.sections[0].content
+    // Here it has not been signed yet.
+    //****** START 5
 
     it('get the Consents document', function (done) {
         store.server
@@ -106,6 +142,12 @@ describe('consent unit', function () {
             });
     });
 
+    //****** END 5
+
+    //******
+    // Sign the Consent Form.
+    //****** START 6
+
     it('sign the Consents document', function (done) {
         store.server
             .post(`/api/v1.0/consent-signatures`)
@@ -119,6 +161,14 @@ describe('consent unit', function () {
                 done();
             });
     });
+
+    //****** END 6
+
+    //******
+    // Get the Consent Form and check if it is signed.\
+    // Here it has now been signed yet.
+    //****** START 7
+
 
     it('get the Consents document', function (done) {
         store.server
@@ -136,6 +186,8 @@ describe('consent unit', function () {
                 done();
             });
     });
+
+    //****** END 7
 
     it('logout as user', shared.logoutFn(store));
 });
