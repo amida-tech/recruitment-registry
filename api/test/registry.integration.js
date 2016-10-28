f3311f4a6e074583ec2a87086b93c3d9421f44ef
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-10-28 11:12:41 -0400

    test cleaning

--- api/test/registry.integration.js
+++ api/test/registry.integration.js
@@ -11,7 +11,7 @@ const SharedIntegration = require('./util/shared-integration');
 const History = require('./util/entity-history');
 const Generator = require('./util/entity-generator');
 const comparator = require('./util/client-server-comparator');
-//const ConsentDocumentHistory = require('./util/consent-document-history');
+const ConsentDocumentHistory = require('./util/consent-document-history');
 
 const expect = chai.expect;
 const generator = new Generator();
@@ -26,7 +26,7 @@ describe('registry integration', function () {
     const hxSurvey = new History(['id', 'name']);
     const hxUser = new History();
     const hxAnswers = [];
-    //const hxConsentDoc = new ConsentDocumentHistory(2);
+    const hxConsentDoc = new ConsentDocumentHistory(2);
 
     before(shared.setUpFn(store));
 
@@ -56,127 +56,131 @@ describe('registry integration', function () {
 
     it('login as super', shared.loginFn(store, config.superUser));
 
-    it('create profile survey', function (done) {
-        const clientSurvey = generator.newSurvey();
-        store.server
-            .post('/api/v1.0/profile-survey')
-            .set('Authorization', store.auth)
-            .send(clientSurvey)
-            .expect(201)
-            .end(function (err, res) {
-                if (err) {
-                    return done(err);
-                }
-                hxSurvey.push(clientSurvey, res.body);
-                done();
-            });
-    });
+    for (let i = 0; i < 2; ++i) {
+        it(`create consent type ${i}`, shared.createConsentTypeFn(store, hxConsentDoc));
+    }
+
+    for (let i = 0; i < 2; ++i) {
+        it(`create consent document of type ${i}`, shared.createConsentDocumentFn(store, hxConsentDoc, i));
+    }
+
+    const createProfileSurveyFn = function () {
+        return function (done) {
+            const clientSurvey = generator.newSurvey();
+            store.server
+                .post('/api/v1.0/profile-survey')
+                .set('Authorization', store.auth)
+                .send(clientSurvey)
+                .expect(201)
+                .end(function (err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+                    hxSurvey.push(clientSurvey, res.body);
+                    done();
+                });
+        };
+    };
+
+    it('create profile survey', createProfileSurveyFn());
 
     it('logout as super', shared.logoutFn(store));
 
-    it(`get profile survey`, function (done) {
-        store.server
-            .get('/api/v1.0/profile-survey')
-            .expect(200)
-            .end(function (err, res) {
-                if (err) {
-                    return done(err);
-                }
-                const index = 0;
-                const id = hxSurvey.id(index);
-                expect(res.body.id).to.equal(id);
-                hxSurvey.updateServer(index, res.body);
-                comparator.survey(hxSurvey.client(index), res.body)
-                    .then(done, done);
-            });
-    });
-
-    it('fill user profile and submit', function (done) {
-        const surveyIndex = 0;
-        //const signatures = [0];
-        const survey = hxSurvey.server(surveyIndex);
-        const clientUser = generator.newUser();
-        clientUser.role = 'participant';
-        const answers = generator.answerQuestions(survey.questions);
-        hxAnswers.push(answers);
-        const input = { user: clientUser, answers };
-        //if (signatures) {
-        //    input.signatures = signatures.map(sign => hxConsentDoc.id(sign));
-        //}
-        store.server
-            .post('/api/v1.0/profiles')
-            .send(input)
-            .expect(201)
-            .end(function (err, res) {
-                if (err) {
-                    return done(err);
-                }
-                store.auth = 'Bearer ' + res.body.token;
-                hxUser.push(clientUser, {});
-                done();
-            });
-    });
-
-    it('verify user profile', function (done) {
-        const surveyIndex = 0;
-        const userIndex = 0;
-        store.server
-            .get('/api/v1.0/profiles')
-            .set('Authorization', store.auth)
-            .expect(200)
-            .end(function (err, res) {
-                if (err) {
-                    return done(err);
-                }
-                const result = res.body;
-                const survey = hxSurvey.server(surveyIndex);
-
-                comparator.user(hxUser.client(userIndex), result.user);
-                comparator.answeredSurvey(survey, hxAnswers[userIndex], result.survey);
-
-                done();
-            });
-    });
-
-    it('update user profile', function (done) {
-        const surveyIndex = 0;
-        const userIndex = 0;
-        const survey = hxSurvey.server(surveyIndex);
-        const answers = generator.answerQuestions(survey.questions);
-        const userUpdates = {
-            email: `updated${userIndex}@example.com`
+    const verifyProfileSurveyFn = function () {
+        return function (done) {
+            store.server
+                .get('/api/v1.0/profile-survey')
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+                    const index = 0;
+                    const id = hxSurvey.id(index);
+                    expect(res.body.id).to.equal(id);
+                    hxSurvey.updateServer(index, res.body);
+                    comparator.survey(hxSurvey.client(index), res.body)
+                        .then(done, done);
+                });
         };
-        hxUser.client(userIndex).email = userUpdates.email;
-        const updateObj = {
-            user: userUpdates,
-            answers
+    };
+
+    it(`get profile survey`, verifyProfileSurveyFn());
+
+    const createProfileFn = function (surveyIndex) {
+        return function (done) {
+            //const signatures = [0];
+            const survey = hxSurvey.server(surveyIndex);
+            const clientUser = generator.newUser();
+            clientUser.role = 'participant';
+            const answers = generator.answerQuestions(survey.questions);
+            hxAnswers.push(answers);
+            const input = { user: clientUser, answers };
+            //if (signatures) {
+            //    input.signatures = signatures.map(sign => hxConsentDoc.id(sign));
+            //}
+            store.server
+                .post('/api/v1.0/profiles')
+                .send(input)
+                .expect(201)
+                .end(function (err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+                    store.auth = 'Bearer ' + res.body.token;
+                    hxUser.push(clientUser, {});
+                    done();
+                });
         };
-        hxAnswers[userIndex] = answers;
-        store.server
-            .patch('/api/v1.0/profiles')
-            .set('Authorization', store.auth)
-            .send(updateObj)
-            .expect(204, done);
-    });
+    };
 
-    it('verify user profile', function (done) {
-        const surveyIndex = 0;
-        const userIndex = 0;
-        store.server
-            .get('/api/v1.0/profiles')
-            .set('Authorization', store.auth)
-            .expect(200)
-            .end(function (err, res) {
-                if (err) {
-                    return done(err);
-                }
-                const result = res.body;
-                const survey = hxSurvey.server(surveyIndex);
+    const verifyProfileFn = function (surveyIndex, userIndex) {
+        return function (done) {
+            store.server
+                .get('/api/v1.0/profiles')
+                .set('Authorization', store.auth)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+                    const result = res.body;
+                    const survey = hxSurvey.server(surveyIndex);
 
-                comparator.user(hxUser.client(userIndex), result.user);
-                comparator.answeredSurvey(survey, hxAnswers[userIndex], result.survey);
+                    comparator.user(hxUser.client(userIndex), result.user);
+                    comparator.answeredSurvey(survey, hxAnswers[userIndex], result.survey);
 
-                done();
-            });
-    });
+                    done();
+                });
+        };
+    };
+
+    const updateProfileFn = function (surveyIndex, userIndex) {
+        return function (done) {
+            const survey = hxSurvey.server(surveyIndex);
+            const answers = generator.answerQuestions(survey.questions);
+            const userUpdates = {
+                email: `updated${userIndex}@example.com`
+            };
+            hxUser.client(userIndex).email = userUpdates.email;
+            const updateObj = {
+                user: userUpdates,
+                answers
+            };
+            hxAnswers[userIndex] = answers;
+            store.server
+                .patch('/api/v1.0/profiles')
+                .set('Authorization', store.auth)
+                .send(updateObj)
+                .expect(204, done);
+        };
+    };
+
+    it('register user 0 with profile survey 0', createProfileFn(0));
+
+    it('verify user 0 profile', verifyProfileFn(0, 0));
+
+    it('update user 0 profile', updateProfileFn(0, 0));
+
+    it('verify user 0 profile', verifyProfileFn(0, 0));
 });
