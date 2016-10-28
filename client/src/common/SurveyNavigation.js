620030487a55fa080d550d6cf67a4d132205a66b
james <jmfadeley@gmail.com>
2016-10-28 17:33:31 -0400

    Forgot navigation.

---
+++ client/src/common/SurveyNavigation.js
@@ -0,0 +1,28 @@
+import React, { Component} from 'react';
+
+export class SurveyNavigator extends Component {
+  render() {
+    var temp ="btn rr-button m-r-2";
+    return(
+        <div id={this.props.id} className="rr">
+          {this.props.location == 8 ? (
+            <div className="rr-wrapper m-b-2">
+              <h1 className="rr no-transform">{this.props.vocab.get('LETS_CREATE')}</h1>
+            </div>
+          ) : (<div></div>)}
+          {this.props.surveyField}
+          <div className="rr-controls">
+            <button className= {this.props.location == 1 ? "invisible " + temp : temp}
+            onClick={this.props.previous} type="button">
+              {this.props.vocab.get('BACK')}
+            </button>
+            {this.props.location != 8 ?
+              (<button className={temp} onClick={this.props.next} type="button">
+              {this.props.vocab.get('NEXT')}</button>) :
+              (<button className={temp} onClick={this.props.next} type="button">
+              {this.props.vocab.get('REGISTER')}</button>)}
+          </div>
+        </div>
+      )
+  }
+}
