cc90b9352f3ea8772ace2fcd26cc6e6c610f1ec4
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-10-29 11:15:02 -0400

    match controllers with paths one to one

--- api/controllers/profile-survey.controller.js
+++ api/controllers/profile-survey.controller.js
@@ -27,21 +27,3 @@ exports.updateProfileSurveyText = function (req, res) {
         .then(result => res.status(204).json(result))
         .catch(shared.handleError(res));
 };
-
-exports.createProfile = function (req, res) {
-    Registry.createProfile(req.body)
-        .then(tokenObj => res.status(201).json(tokenObj))
-        .catch(shared.handleError(res));
-};
-
-exports.updateProfile = function (req, res) {
-    Registry.updateProfile(req.user.id, req.body)
-        .then(() => res.status(204).end())
-        .catch(shared.handleError(res));
-};
-
-exports.getProfile = function (req, res) {
-    Registry.getProfile({ userId: req.user.id })
-        .then(result => res.status(200).json(result))
-        .catch(shared.handleError(res));
-};
