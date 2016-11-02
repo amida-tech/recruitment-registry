8642dee70da82ac7af0615f6c842ff6c81a2bd66
Sean Bolak <seanbolak@gmail.com>
2016-11-02 16:40:14 -0400

    RR-83 Login Pod Interface refactor

--- client/src/login/reducer.js
+++ client/src/login/reducer.js
@@ -1,31 +1,16 @@
-import * as actionTypes from './actionTypes'
-import Immutable from 'immutable'
+import * as actionTypes from './actionTypes';
 
-export const initialState = {
-  formState: {
-    username: '',
-    password: '',
-  },
-  user: localStorage.user ? JSON.parse(localStorage.user) : {
-    username: "",
-    role: "",
-    id: ""
-  }
-}
-
-var immutableState = Immutable.fromJS(initialState);
-
-export default (state = immutableState, action) => {
+export default (state, action) => {
   switch (action.type) {
     case actionTypes.UPDATE_CREDENTIALS:
-      return state.setIn(['formState', action.name], action.value)
+      return state.setIn(['formState', action.name], action.value);
     case actionTypes.LOGIN_ERROR:
-      return state.setIn(['formState', 'hasErrors'], true)
+      return state.setIn(['formState', 'hasErrors'], true);
     case actionTypes.LOGIN_SUCCESS:
-      return state.setIn(['formState', 'hasErrors'], false)
+      return state.setIn(['formState', 'hasErrors'], false);
     case actionTypes.GET_USER_SUCCESS:
-      localStorage.user = JSON.stringify(action.payload)
-      return state.set('user', Immutable.fromJS(action.payload))
+      localStorage.user = JSON.stringify(action.payload);
+      return state.set('user', action.payload);
     default:
       return state;
   }
