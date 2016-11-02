4e919d629af22d4995928bba58e13ffefe233d85
Sean Bolak <seanbolak@gmail.com>
2016-11-02 16:43:50 -0400

    Almost forgot to change the reference to the component in routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,5 @@
 import Home from './home/index';
-import Login from './login/components/index';
+import { LoginContainer } from './login';
 import { RegisterContainer } from './register';
 import { ProfileContainer } from './profile';
 import SurveyBuilderContainer from './surveyBuilder/components/index';
@@ -8,7 +8,7 @@ import { SurveyContainer } from './survey';
 
 export default [
   { path: '/', title: 'Home', transTerm: 'HOME', component: Home, requiresAuth: false },
-  { path: '/login', title: 'Login', transTerm: 'LOGIN', component: Login, requiresAuth: false, newUsers: true },
+  { path: '/login', title: 'Login', transTerm: 'LOGIN', component: LoginContainer, requiresAuth: false, newUsers: true },
   { path: '/register', title: 'Register', transTerm: 'REGISTER', component: RegisterContainer, requiresAuth: false, newUsers: true },
   { path: '/profile', title: 'Profile', transTerm: 'PROFILE', component: ProfileContainer, requiresAuth: true, newUsers: false },
   { path: '/survey-builder(/:id)', title: 'Survey Builder', transTerm: 'SURVEY_BUILDER', component: SurveyBuilderContainer, requiresAuth: true, newUsers: false, isSuper: true },
