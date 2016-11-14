d335020131fa7246dca520a0e359b258f661d048
Sean Bolak <seanbolak@gmail.com>
2016-11-14 10:24:39 -0500

    added Link tag to button and created consent pod.

---
+++ client/src/consent/components/index.js
@@ -0,0 +1,18 @@
+import React { Component } from 'react';
+import { connect } from 'react-redux';
+import Consent from './Consent';
+
+class ConsentContainer extends Component {
+  render() {
+    return <Consent />
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    data: state,
+    ...ownProps
+  }
+};
+
+export default connect(mapStateToProps)(ConsentContainer);
