5c5ed5bd6f297f188b0776204e22f8411bed68bc
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-11-22 09:27:37 -0500

    read pillars and questions.  create indices for pillars.

--- api/import/ccf.js
+++ api/import/ccf.js
@@ -1,14 +1,54 @@
 'use strict';
 
+const _ = require('lodash');
+
 const SPromise = require('../lib/promise');
 
 const CSVConverter = require('./csv-converter');
 
+const options = {
+	questions: {
+		ignoreEmpty: true,
+		headers: [
+			'id', 'key', 'text', 'instruction', 'skipCount', 'type',
+			'conditionalKey', 'choice', 'answerKey', 'tag', 'toggle'
+		]
+	},
+};
+
+const transform = {
+	questions(input) {
+		return input;
+	},
+};
+
+const postAction = {
+	pillars(result, key, json) {
+		result[key] = json;
+		result[`${key}_id_index`] = _.keyBy(json, 'id');
+		result[`${key}_title_index`] = _.keyBy(json, 'title');
+	}
+};
+
 const importFile = function (filepaths, result, key) {
     const filepath = filepaths[key];
-    const converter = new CSVConverter();
+    const keyOptions = options[key] || {};
+    const converter = new CSVConverter(keyOptions);
     return converter.fileToRecords(filepath)
-        .then(json => result[key] = json);
+        .then(json => {
+        	const fn = transform[key];
+        	if (fn) {
+        		return fn(json);
+        	}
+        	return json;
+        })
+        .then(json => {
+        	const fn = postAction[key];
+        	if (fn) {
+        		return fn(result, key, json);
+        	}
+        	result[key] = json;
+        });
 };
 
 const importFiles = function (filepaths) {
