af4c5a63ee4b44e99cdc4e2c22aa3e91ff429d1c
Jacob Sachs <jacob.s.sachs@gmail.com>
2016-12-07 12:50:45 -0500

    add seed script to startup process

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const childProcess = require('child_process');
+
 const config = require('./config');
 
 const app = require('./app');
@@ -9,10 +11,39 @@ appgen.initialize(app, {}, function (err) {
     if (err) {
         console.log('Server failed to start due to error: %s', err);
     } else {
-        app.listen(config.port, function () {
-            console.log('Server started at ', config.port);
+        runScript(function () {
+            app.listen(config.port, function () {
+                console.log('Server started at ', config.port);
+            });
         });
     }
 });
 
 module.exports = app;
+
+/**
+ * Helper function to run the seed script on server start
+ */
+function runScript(callback) {
+
+    // keep track of whether callback has been invoked to prevent multiple invocations
+    var invoked = false;
+
+    var process = childProcess.fork(process.env.RECREG_STARTUP_SCRIPT);
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    // execute the callback once the process has finished running
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        var err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+
+}
