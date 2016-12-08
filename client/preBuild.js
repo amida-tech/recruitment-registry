b15ddeae4d74ee5eb102456c5c694988aaef1b22
Sean Bolak <seanbolak@gmail.com>
2016-12-08 17:17:26 -0500

    fixed preBuild.js message

--- client/preBuild.js
+++ client/preBuild.js
@@ -7,5 +7,5 @@ fs.writeFile(
     'src/config/production.js',
     "export default { NODE_ENV: 'production', API_HTTP_URL:'" + process.env.PROD_API_HTTP_URL + "', API_HTTPS_URL:'" + process.env.PROD_API_HTTPS_URL + "'}",
     function(response) {
-     console.log(response);
+     console.log("production config created");
     });
