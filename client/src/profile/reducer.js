9a8515d91119159eed534045a95c259e4865a081
Kevin Moore <k.james.moore@gmail.com>
2016-12-13 03:03:57 -0500

    Merge state correctly on GET_PROFILE_SUCCESS action.

--- client/src/profile/reducer.js
+++ client/src/profile/reducer.js
@@ -3,7 +3,10 @@ import Immutable from 'immutable'
 
 export const initialState = {
   user: {
-    name: ""
+    username: "",
+    email: "",
+    role: ""
+
   },
   survey: {
     questions: []
@@ -21,7 +24,14 @@ export default (state = immutableState, action) => {
         .set('userUpdated', undefined)
         .set('profileSaved', true)
     case t.GET_PROFILE_SUCCESS:
-      return Immutable.fromJS(action.payload)
+      // return Immutable.fromJS(action.payload)
+          return state.merge({
+            user : {
+                'username': action.payload.username,
+                'email': action.payload.email,
+                'role': action.payload.role
+            }
+          });
     default:
       return state;
   }
