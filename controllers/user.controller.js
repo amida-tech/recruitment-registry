6233ca9664fa643bc43bbb267ebfb50563431e2c
Kevin Moore <k.james.moore@gmail.com>
2017-02-03 10:27:13 -0500

    Modularize and DRY up registration email code

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,122 +1,11 @@
 'use strict';
 
 const _ = require('lodash');
-const request = require('request');
 
 const models = require('../models');
 const shared = require('./shared.js');
 
-const config = require('../config');
-
-
-// IMPORT REQUESTS TO CALL CONSTANT CONTACT API
-
-const CC_BASE_API_URL = "https://api.constantcontact.com/v2";
-const emailUrl = CC_BASE_API_URL + "/contacts?action_by=ACTION_BY_VISITOR";
-const apiKeyQueryParam = "&api_key=";
-
-
-
-
-const newContactJSON = {
-    "first_name": "Amida",
-    "last_name": "Amida",
-    "lists": [
-        {
-            "id": "1"
-        }
-    ],
-    "email_addresses": [
-        {
-            "email_address": "kevin@amida-tech.com"
-        }
-    ]
-};
-
-function checkContactOptions(email) {
-    return {
-        method: 'GET',
-        url: CC_BASE_API_URL + "/contacts",
-        headers: {
-            "Authorization": "Bearer " + config.constantContact.token
-        },
-        qs: {
-            api_key: config.constantContact.apiKey,
-            email: email,
-            access_token: "Bearer " + config.constantContact.token
-        }
-    };
-}
-
-function addNewContactOptions (email) {
-    return {
-        method: 'POST',
-        url: CC_BASE_API_URL + "/contacts",
-        headers: {
-            "Authorization": "Bearer " + config.constantContact.token
-        },
-        qs: {
-            api_key: config.constantContact.apiKey,
-            access_token: "Bearer " + config.constantContact.token,
-            action_by: "ACTION_BY_VISITOR"
-        },
-        json: {
-            "email_addresses": [
-                {
-                    "email_address": email
-                }
-            ],
-            "lists": [
-                {
-                    "id": config.constantContact.listId
-                }
-            ]
-        }
-    };
-}
-
-
-function sendCcEmail (email) {
-
-    console.log("This is working: ", email);
-
-    const firstCall = checkContactOptions(email);
-    const secondCall = addNewContactOptions(email);
-
-
-    // Hit up CC API to make sure user doesn't already exist
-    // request(
-    //     // url,
-    //     firstCall,
-    //     function (error, response, body) {
-    //         if (!error && response.statusCode == 200) {
-    //             console.log("GREAT SUCCESS");
-    //             console.log(body);
-    //         } else {
-    //             console.log("FAIL");
-    //             console.log(response.statusCode);
-    //             console.log(response.statusMessage)
-    //         }
-    //     }
-    // );
-
-    request(
-        // url,
-        secondCall,
-        function (error, response, body) {
-            if (!error && response.statusCode == 201) {
-                console.log("GREAT SUCCESS");
-                console.log(body);
-            } else {
-                console.log("FAIL");
-                console.log(response.statusCode);
-                console.log(response.statusMessage)
-            }
-        }
-    );
-
-};
-
+const sendMail = require('../lib/email');
 
 
 
@@ -125,14 +14,10 @@ exports.createNewUser = function (req, res) {
 
     return models.user.createUser(newUser)
         .then(({ id }) => {
-            console.log("New user created");
-            console.log(newUser);
-            sendCcEmail(newUser.email);
-
+            sendMail(newUser, 'new_contact', {});
             res.status(201).json({ id })
         })
         .catch((res) => {
-                console.log("Failed");
                 shared.handleError(res)
             }
         );
