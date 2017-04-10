0d440696118a2450e53269b28ff6a4935de2bca6
Afsin Ustundag <afsin@amida.com>
2017-04-10 11:02:26 -0400

    add a seed script to add survey translations to db

--- syncAndLoadAlzheimer.js
+++ syncAndLoadAlzheimer.js
@@ -7,11 +7,14 @@ const models = require('./models');
 const surveyExamples = require('./test/fixtures/example/survey');
 const userExamples = require('./test/fixtures/example/user');
 const consentSeed = require('./test/util/consent-seed');
+const translatedSurveySeed = require('./test/util/translatedSurveySeed');
 const consentExample = require('./test/fixtures/example/consent-demo');
 
 const userExample = userExamples.Alzheimer;
 
 const helper = require('./test/util/survey-common');
+
+const qxTranslations = surveyExamples.exampleQxTranslation;
 
 models.sequelize.sync({ force: true })
     .then(() => models.profileSurvey.createProfileSurvey(surveyExamples.alzheimer))
@@ -25,6 +28,7 @@ models.sequelize.sync({ force: true })
         });
     })
     .then(() => models.survey.createSurvey(surveyExamples.example))
+    .then(id => translatedSurveySeed(id, surveyExamples.exampleTranslation, qxTranslations))
     .then(() => consentSeed(consentExample))
     .then(() => {
         console.log('success');
