b515e26235bc5f12a60b6b16ea5166bcb9afe680
Unprofessional <unprofessionalmadman@gmail.com>
2018-01-11 23:39:00 -0500

    Improve robustness of listDemographics sequelize query

--- models/dao/demographics.dao.js
+++ models/dao/demographics.dao.js
@@ -13,84 +13,98 @@ module.exports = class DemographicsDAO extends Base {
             role = { [Op.in]: ['admin', 'clinician', 'participant'] };
         }
         const where = { role };
-        return this.db.Answer.findAll({
+        return this.db.ProfileSurvey.findAll({
             raw: true,
             attributes: [
-                'userId',
-                'value',
-            ],
-            include: [
-                {
-                    model: this.db.User,
-                    as: 'user',
-                    raw: true,
-                    attributes: [
-                        'createdAt',
-                    ],
-                    where,
-                },
-                {
-                    model: this.db.Question,
-                    as: 'question',
-                    raw: true,
-                    attributes: [
-                        'type',
-                    ],
-                    where: {
-                        type: [
-                            'zip',
-                            'year',
-                        ]
-                    },
-                },
-                {
-                    model: this.db.ProfileSurvey,
-                    as: 'profileSurvey',
-                    raw: true,
-                    attributes: [],
-                    where: {
-                        surveyId: {
-                            // NOTE: There could possibly be multiple ProfileSurveys?
-                            [Op.ne]: null,
-                        },
-                    },
-                },
+                'id',
             ],
         })
-        .then((demographics) => {
-            demographics = demographics.map((demographic) => {
-                const formattedDemographic = {
-                    userId: demographic.userId,
-                };
-                if(demographic['question.type'] === 'zip') {
-                    formattedDemographic.zip = demographic.value;
-                }
-                if(demographic['question.type'] === 'year') {
-                    formattedDemographic.yob = demographic.value;
-                }
-                formattedDemographic.registrationDate = moment(demographic['user.createdAt'],'YYYY-MM-DD')
-                    .format('YYYY-MM-DD');
-                return formattedDemographic;
-            });
-            demographics = _.chain(demographics)
-                .groupBy('userId')
-                .map((userRecordSet) => {
-                    const zipRecord = userRecordSet.find((record) => record.zip);
-                    const yobRecord = userRecordSet.find((record) => record.yob);
-                    const unifiedRecord = Object.assign({},
-                        zipRecord,
-                        yobRecord
-                    );
-                    const anonymizedUnifiedRecord = {
-                        zip: unifiedRecord.zip,
-                        yob: unifiedRecord.yob,
-                        registrationDate: unifiedRecord.registrationDate,
-                    };
-                    return anonymizedUnifiedRecord;
+        .then((surveyIds) => {
+            surveyIds = surveyIds.map((surveyId) => surveyId.id);
+            return this.db.SurveyQuestion.findAll({
+                raw: true,
+                attributes: [
+                    'id',
+                ],
+                where: {
+                    surveyId: surveyIds,
+                },
+            })
+            .then((questionIds) => {
+                questionIds = questionIds.map((questionId) => questionId.id);
+                return this.db.Answer.findAll({
+                    raw: true,
+                    attributes: [
+                        'value',
+                    ],
+                    where: {
+                        questionId: questionIds,
+                    },
+                    include: [
+                        {
+                            model: this.db.User,
+                            as: 'user',
+                            raw: true,
+                            attributes: [
+                                'id',
+                                'createdAt',
+                            ],
+                            where,
+                        },
+                        {
+                            model: this.db.Question,
+                            as: 'question',
+                            raw: true,
+                            attributes: [
+                                'type',
+                            ],
+                            where: {
+                                type: [
+                                    'zip',
+                                    'year',
+                                ],
+                            },
+                        },
+                    ],
                 })
-                .flattenDeep()
-                .value();
-            return demographics;
+                .then(demographics => this.formatAndMergeDemographics(demographics));
+            });
         });
+    }
+
+    formatAndMergeDemographics(demographics) {
+        demographics = demographics.map((demographic) => {
+            const formattedDemographic = {
+                userId: demographic['user.id'],
+            };
+            if(demographic['question.type'] === 'zip') {
+                formattedDemographic.zip = demographic.value;
+            }
+            if(demographic['question.type'] === 'year') {
+                formattedDemographic.yob = demographic.value;
+            }
+            formattedDemographic.registrationDate = moment(demographic['user.createdAt'],'YYYY-MM-DD')
+                .format('YYYY-MM-DD');
+            return formattedDemographic;
+        });
+        demographics = _.chain(demographics)
+            .groupBy('userId')
+            .map((userRecordSet) => {
+                const zipRecord = userRecordSet.find((record) => record.zip);
+                const yobRecord = userRecordSet.find((record) => record.yob);
+                const unifiedRecord = Object.assign({},
+                    zipRecord,
+                    yobRecord
+                );
+                const anonymizedUnifiedRecord = {
+                    zip: unifiedRecord.zip,
+                    yob: unifiedRecord.yob,
+                    registrationDate: unifiedRecord.registrationDate,
+                };
+                return anonymizedUnifiedRecord;
+            })
+            .flattenDeep()
+            .value();
+        return demographics;
     }
 };
